eed74c63f2aca6be5b92262bb55e7a95bde7c4c1
Raghav002 <raghavjindal2002@gmail.com>
2022-09-30 20:13:23 +0800

    Changes to main file 2

--- frenchapp.py
+++ frenchapp.py
@@ -9,7 +9,6 @@ from flask_sqlalchemy import SQLAlchemy
 from googletrans import Translator
 from playsound import playsound
 from flask_login import UserMixin, login_user, login_remembered, LoginManager, login_required, logout_user, current_user
-import gtts
 
 # Section 1 - Database Model Construction
 
